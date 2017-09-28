c3d01c569d0979117d0e4356499d9e9b58713ff1
literalnon <donaldjrduck@gmail.com>
2017-09-28 20:23:41 +0200

    initial

---
+++ CG_lab2/CG_lab2/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace CG_lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelColor = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.buttonLoadImg = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.radioButtonImage = new System.Windows.Forms.RadioButton();
+            this.radioButtonColor = new System.Windows.Forms.RadioButton();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(25, 32);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(624, 318);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // labelColor
+            // 
+            this.labelColor.BackColor = System.Drawing.Color.Black;
+            this.labelColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelColor.Location = new System.Drawing.Point(788, 32);
+            this.labelColor.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelColor.Name = "labelColor";
+            this.labelColor.Size = new System.Drawing.Size(66, 61);
+            this.labelColor.TabIndex = 1;
+            this.labelColor.BackColorChanged += new System.EventHandler(this.labelColor_BackColorChanged);
+            this.labelColor.Click += new System.EventHandler(this.labelColor_Click);
+            // 
+            // buttonLoadImg
+            // 
+            this.buttonLoadImg.Location = new System.Drawing.Point(671, 32);
+            this.buttonLoadImg.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonLoadImg.Name = "buttonLoadImg";
+            this.buttonLoadImg.Size = new System.Drawing.Size(67, 54);
+            this.buttonLoadImg.TabIndex = 2;
+            this.buttonLoadImg.Text = "Load Image";
+            this.buttonLoadImg.UseVisualStyleBackColor = true;
+            this.buttonLoadImg.Click += new System.EventHandler(this.buttonLoadImg_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // radioButtonImage
+            // 
+            this.radioButtonImage.AutoSize = true;
+            this.radioButtonImage.Location = new System.Drawing.Point(671, 157);
+            this.radioButtonImage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButtonImage.Name = "radioButtonImage";
+            this.radioButtonImage.Size = new System.Drawing.Size(67, 21);
+            this.radioButtonImage.TabIndex = 3;
+            this.radioButtonImage.Text = "Image";
+            this.radioButtonImage.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonColor
+            // 
+            this.radioButtonColor.AutoSize = true;
+            this.radioButtonColor.Location = new System.Drawing.Point(788, 157);
+            this.radioButtonColor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButtonColor.Name = "radioButtonColor";
+            this.radioButtonColor.Size = new System.Drawing.Size(62, 21);
+            this.radioButtonColor.TabIndex = 4;
+            this.radioButtonColor.Text = "Color";
+            this.radioButtonColor.UseVisualStyleBackColor = true;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(726, 232);
+            this.buttonClear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(65, 54);
+            this.buttonClear.TabIndex = 5;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(614, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Область рисуется левой кнопкой мыши, заливка осуществляется нажатием правой кнопк" +
+    "и";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 363);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.radioButtonColor);
+            this.Controls.Add(this.radioButtonImage);
+            this.Controls.Add(this.buttonLoadImg);
+            this.Controls.Add(this.labelColor);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Filling";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelColor;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button buttonLoadImg;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RadioButton radioButtonImage;
+        private System.Windows.Forms.RadioButton radioButtonColor;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CG_lab2/CG_lab2/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Threading;
+
+namespace CG_lab2
+{
+
+    // Реализовать Рекурсивный алгоритм заливки на основе серий пикселов (линий) в двух вариантах:
+    // 1) заливка заданным цветом
+    // 2) заливка рисунком из графического файла.
+    // Файл можно загрузить встроенными средствами и затем считывать точки изображения для использования в заливке.
+
+    // Область рисуется мышкой. Область произвольной формы. Внутри могут быть отверстия.
+    // Точка, с которой начинается заливка, задается щелчком мыши.
+    public partial class Form1 : Form
+    {
+        private Bitmap bmp;
+        private Graphics g;
+        private Pen pen = new Pen(Color.Black);
+        private Point startPt;
+        private Bitmap img;
+        private int currentColor;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pen.StartCap = pen.EndCap = LineCap.Round;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+            pictureBox1.Refresh();
+            g = Graphics.FromImage(bmp);
+            radioButtonImage.Enabled = false;
+            radioButtonColor.Select();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                g.DrawLine(pen, startPt, e.Location);
+                startPt = e.Location;
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            startPt = e.Location;
+            if (e.Button == MouseButtons.Right)
+            {
+                g = Graphics.FromImage(bmp);
+                currentColor = bmp.GetPixel(e.Location.X, e.Location.Y).ToArgb();
+                if (radioButtonColor.Checked)
+                    FillColor(e.Location);
+                else if (radioButtonImage.Checked)
+                    FillImage(e.Location, new Point(0, 0));
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void labelColor_BackColorChanged(object sender, EventArgs e)
+        {
+            pen.Color = labelColor.BackColor;
+        }
+
+        private bool PointIsFilled(Point p)
+        {
+            //return bmp.GetPixel(p.X, p.Y).ToArgb() == colorDialog1.Color.ToArgb();
+            return bmp.GetPixel(p.X, p.Y).ToArgb() != currentColor;
+        }
+
+        private bool OutOfBorder(Point p)
+        {
+            return !(p.X >= 0 && p.Y >= 0 && p.X < bmp.Width && p.Y < bmp.Height);
+        }
+
+        private int FindLeftBorder(Point p)
+        {
+            int leftBorder = p.X;
+            while (bmp.GetPixel(leftBorder, p.Y).ToArgb() != colorDialog1.Color.ToArgb() && leftBorder > 0)
+                --leftBorder;
+            return leftBorder;
+        }
+
+        private int FindRightBorder(Point p)
+        {
+            int rightBorder = p.X;
+            while (bmp.GetPixel(rightBorder, p.Y).ToArgb() != colorDialog1.Color.ToArgb() && rightBorder < bmp.Width - 1)
+                ++rightBorder;
+            return rightBorder;
+        }
+
+        private void FillColor(Point p)
+        {
+            if (OutOfBorder(p)) return;
+            // Если текущая точка не закрашена
+            if (!PointIsFilled(p))
+            {
+                //Для текущей точки находим левую и правую границу.
+                int l = FindLeftBorder(p);
+                int r = FindRightBorder(p);
+                //Рисуем линию от левой границы до правой границы, не включая саму границу.
+                Point lp = new Point(l+1, p.Y);
+                Point rp = new Point(r-1, p.Y);
+                Pen f = new Pen(pen.Color, 1);
+                g.DrawLine(f, lp, rp);
+                //Thread.Sleep(1);
+                pictureBox1.Refresh();
+                // В цикле от левой до правой границы (не включая саму границу) вызываем эту же функцию рекурсивно для всех точек, лежащих выше текущей на один пиксел.
+                for (int i = l + 1; i < r; ++i)
+                    FillColor(new Point(i, p.Y + 1));
+                // Выполняем аналогичный цикл для всех точек, лежащих ниже текущей на один пиксел.
+                for (int i = l + 1; i < r; ++i)
+                    FillColor(new Point(i, p.Y - 1));
+            }
+        }
+
+        private int IncImgX (int x, int d)
+        {
+            return (x + d) % img.Width;
+        }
+
+        private int DecImgX(int x, int d)
+        {
+            x -= d;
+            while (x < 0)
+                x += img.Width;
+            return x;
+        }
+
+        private int IncImgY (int y, int d)
+        {
+            return (y + d) % img.Height;
+        }
+
+        private int DecImgY(int y, int d)
+        {
+            y -= d;
+            while (y < 0)
+                y += img.Height;
+            return y;
+        }
+
+        // В параметрах передаются соответствующие точки на заливаемой фигуре и на картинке
+        private void DrawLineFromImage(Point figurePoint, Point imgPoint, int lBorder, int rBorder)
+        {
+            //Rectangle rect = new Rectangle(0,0,img.Width,img.Height);
+            //BitmapData bmpData = img.LockBits(rect,ImageLockMode.ReadWrite,img.PixelFormat);
+            //IntPtr ptr = bmpData.Scan0;
+            int imgX = imgPoint.X;
+            // заливаем вправо
+            for (int i = figurePoint.X + 1; i < rBorder; ++i)
+            {
+                imgX = IncImgX(imgX,1);
+                bmp.SetPixel(i, figurePoint.Y, img.GetPixel(imgX, imgPoint.Y));
+            }
+            // заливаем влево
+            imgX = imgPoint.X;
+            for (int i = figurePoint.X; i > lBorder; --i)
+            {
+                imgX = DecImgX(imgX,1);
+                bmp.SetPixel(i, figurePoint.Y, img.GetPixel(imgX, imgPoint.Y));
+            }
+        }
+
+        private void FillImage(Point figurePoint, Point imgPoint)
+        {
+            if (OutOfBorder(figurePoint)) return;
+            // Если текущая точка не закрашена
+            if (!PointIsFilled(figurePoint))
+            {
+                //Для текущей точки находим левую и правую границу.
+                int l = FindLeftBorder(figurePoint);
+                int r = FindRightBorder(figurePoint);
+                //Рисуем линию от левой границы до правой границы, не включая саму границу.
+                Point lp = new Point(l + 1, figurePoint.Y);
+                Point rp = new Point(r - 1, figurePoint.Y);
+                Pen f = new Pen(pen.Color, 1);
+                DrawLineFromImage(figurePoint,imgPoint,l,r);
+                //Thread.Sleep(1);
+                pictureBox1.Refresh();
+                // В цикле от левой до правой границы (не включая саму границу) вызываем эту же функцию рекурсивно для всех точек, лежащих выше текущей на один пиксел.
+                // Слева и ниже текущей
+                int d = 0;
+                for (int i = figurePoint.X; i > l; --i)
+                {
+                    FillImage(new Point(i, figurePoint.Y + 1), new Point(DecImgX(imgPoint.X, d), IncImgY(imgPoint.Y, 1)));
+                    ++d;
+                }
+                // слева выше
+                d = 0;
+                for (int i = figurePoint.X; i > l; --i)
+                {
+                    FillImage(new Point(i, figurePoint.Y - 1), new Point(DecImgX(imgPoint.X, d), DecImgY(imgPoint.Y, 1)));
+                    ++d;
+                }
+                // Справа и ниже текущей
+                d = 0;
+                for (int i = figurePoint.X; i < r; ++i)
+                {
+                    FillImage(new Point(i, figurePoint.Y + 1), new Point(IncImgX(imgPoint.X, d), IncImgY(imgPoint.Y, 1)));
+                    ++d;
+                }
+                // Выполняем аналогичный цикл для всех точек, лежащих ниже текущей на один пиксел.
+                // справа выше
+                d = 0;
+                for (int i = figurePoint.X; i < r; ++i)
+                {
+                    FillImage(new Point(i, figurePoint.Y - 1), new Point(IncImgX(imgPoint.X, d), DecImgY(imgPoint.Y, 1)));
+                    ++d;
+                }
+            }
+        }
+
+        private void labelColor_Click(object sender, EventArgs e)
+        {
+            colorDialog1.Color = labelColor.BackColor;
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                labelColor.BackColor = colorDialog1.Color;
+        }
+
+        private void buttonLoadImg_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                img = Bitmap.FromFile(openFileDialog1.FileName) as Bitmap;
+                radioButtonImage.Enabled = true;
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            g.Clear(pictureBox1.BackColor);
+            pictureBox1.Refresh();
+        }
+    }
+}
